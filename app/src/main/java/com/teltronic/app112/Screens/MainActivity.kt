90dc2cdc1c409c22168c04acb6133958f4f9cfe6
Anthonny Pazmiño <anthonny17junio@gmail.com>
2020-02-06 12:56:54 +0100

    Enable dataBinding

--- app/src/main/java/com/teltronic/app112/Screens/MainActivity.kt
+++ app/src/main/java/com/teltronic/app112/Screens/MainActivity.kt
@@ -2,12 +2,16 @@ package com.teltronic.app112.Screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.teltronic.app112.R
+import com.teltronic.app112.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }

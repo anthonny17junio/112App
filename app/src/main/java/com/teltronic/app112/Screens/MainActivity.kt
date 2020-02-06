6d6397572de953342b75254cda3e4d25a3229591
Anthonny Pazmiño <anthonny17junio@gmail.com>
2020-02-06 12:44:22 +0100

    Initial commit

---
+++ app/src/main/java/com/teltronic/app112/Screens/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.teltronic.app112.Screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teltronic.app112.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

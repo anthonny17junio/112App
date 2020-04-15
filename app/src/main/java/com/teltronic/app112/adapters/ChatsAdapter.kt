e7c83dd7157532f85c3229d37c7282aca0aca047
Anthonny Pazmiño <anthonny17junio@gmail.com>
2020-04-15 22:51:55 +0200

    add data binding to chats adapter

--- app/src/main/java/com/teltronic/app112/adapters/ChatsAdapter.kt
+++ app/src/main/java/com/teltronic/app112/adapters/ChatsAdapter.kt
@@ -3,32 +3,21 @@ package com.teltronic.app112.adapters
 import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.teltronic.app112.R
 import com.teltronic.app112.classes.enums.ChatState
 import com.teltronic.app112.classes.enums.Subcategory
 import com.teltronic.app112.database.room.chats.ChatEntity
+import com.teltronic.app112.databinding.ItemChatBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
 //  class ChatsAdapter(private val context: Context) : RecyclerView.Adapter<ChatsAdapter.ViewHolder>() {
-    class ChatsAdapter(private val context: Context) : ListAdapter<ChatEntity, ChatsAdapter.ViewHolder>(ChatsDiffCallback()) {
-
-//    var data = listOf<ChatEntity>()
-//        set(value) {
-//            field = value
-//            notifyDataSetChanged()
-//        }
-
-//    override fun getItemCount() = data.size
-
+class ChatsAdapter(private val context: Context) :
+    ListAdapter<ChatEntity, ChatsAdapter.ViewHolder>(ChatsDiffCallback()) {
 
     @SuppressLint("SimpleDateFormat")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -40,21 +29,16 @@ import java.util.*
         return ViewHolder.from(this, parent)
     }
 
-    class ViewHolder private constructor(itemView: View, private val context: Context) :
-        RecyclerView.ViewHolder(itemView) {
-        private val ivChatState: ImageView = itemView.findViewById(R.id.ivChatState)
-        private val tvChatState: TextView = itemView.findViewById(R.id.tvChatState)
-        private val tvSubcategory: TextView = itemView.findViewById(R.id.tvSubcategory)
-        private val tvCategory: TextView = itemView.findViewById(R.id.tvCategory)
-        private val tvLocation: TextView = itemView.findViewById(R.id.tvLocation)
-        private val tvDate: TextView = itemView.findViewById(R.id.tvDate)
-        private val tvHour: TextView = itemView.findViewById(R.id.tvHour)
-
+    class ViewHolder private constructor(
+        val binding: ItemChatBinding,
+        private val context: Context
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
         companion object {
             fun from(chatsAdapter: ChatsAdapter, parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.item_chat, parent, false)
-                return ViewHolder(view, chatsAdapter.context)
+                val binding = ItemChatBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding, chatsAdapter.context)
             }
         }
 
@@ -77,22 +61,22 @@ import java.util.*
             val sdfHour = SimpleDateFormat("HH:mm")
             val creationDate = Date(chat.creation_epoch_time.toLong() * 1000)
 
-            tvCategory.text = strCategory
-            tvSubcategory.text = strSubcategory
-            tvLocation.text = chat.location
-            tvDate.text = sdfDate.format(creationDate)
-            tvHour.text = sdfHour.format(creationDate)
+            binding.tvCategory.text = strCategory
+            binding.tvSubcategory.text = strSubcategory
+            binding.tvLocation.text = chat.location
+            binding.tvDate.text = sdfDate.format(creationDate)
+            binding.tvHour.text = sdfHour.format(creationDate)
 
             chatState?.let {
-                ivChatState.setImageResource(chatState.idIcon)
-                tvChatState.text = context.getText(chatState.idTitle)
-                ivChatState.setColorFilter(
+                binding.ivChatState.setImageResource(chatState.idIcon)
+                binding.tvChatState.text = context.getText(chatState.idTitle)
+                binding.ivChatState.setColorFilter(
                     ContextCompat.getColor(
                         context,
                         chatState.idColor
                     )
                 )
-                tvChatState.setTextColor(
+                binding.tvChatState.setTextColor(
                     ContextCompat.getColor(
                         context,
                         chatState.idColor

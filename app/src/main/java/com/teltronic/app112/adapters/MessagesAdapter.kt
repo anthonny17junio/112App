172e7c2c374b266837db5985bcf4640513fa0065
Anthonny Pazmiño <anthonny17junio@gmail.com>
2020-04-27 21:24:39 +0200

    RecyclerView messages with adapter (2)

--- app/src/main/java/com/teltronic/app112/adapters/MessagesAdapter.kt
+++ app/src/main/java/com/teltronic/app112/adapters/MessagesAdapter.kt
@@ -1,32 +1,45 @@
 package com.teltronic.app112.adapters
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.teltronic.app112.R
 import com.teltronic.app112.database.room.messages.MessageEntity
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MessagesAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+class MessagesAdapter : RecyclerView.Adapter<MessagesAdapter.ViewHolder>() {
     var data = listOf<MessageEntity>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
+        val view = layoutInflater.inflate(R.layout.item_message_text_other, parent, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    @SuppressLint("SimpleDateFormat")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.content
+
+        val sdfHour = SimpleDateFormat("HH:mm")
+        val creationDate = Date(item.creation_epoch_time.toLong() * 1000)
+
+        holder.tvMessage.text = item.content
+        holder.tvHour.text = sdfHour.format(creationDate)
     }
 
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvMessage: TextView = itemView.findViewById(R.id.tvMessage)
+        val tvHour: TextView = itemView.findViewById(R.id.tvHour)
+    }
 }
 
-class TextItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
